4920ec1e37f60ecff7bb50fdef0cde663409bd5f
javierpcl <javierpcl@ext.inditex.com>
2019-09-26 09:09:59 +0200

    lecture 4

---
+++ lecture 4/Huffman Coding/sdfsdf/src/main/scala/dsfsdf/App.scala
@@ -0,0 +1,9 @@
+package dsfsdf
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ lecture 4/Huffman Coding/sdfsdf/src/test/scala/dsfsdf/AppTest.scala
@@ -0,0 +1,17 @@
+package dsfsdf
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
